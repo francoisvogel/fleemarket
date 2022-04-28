4a3826b807fceb8f2d981422efd3099f8f0c1483
francoisvogel <francois.g.vogel@gmail.com>
2022-04-28 11:50:05 +0200

    added head elements

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import '../styles/global.css'
 
 export default function MyApp({Component, pageProps}) {
     return (
         <>
+            <Head>
+                <title>Flee Market</title>
+                <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" />
+            </Head>
             <Component {...pageProps} />
         </>
     )
