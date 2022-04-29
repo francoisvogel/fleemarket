793d0728a6320904605385bd5245de52ac3339ab
francoisvogel <francois.g.vogel@gmail.com>
2022-04-29 11:31:48 +0200

    update

--- components/person.jsx
+++ components/person.jsx
@@ -1,8 +1,9 @@
 export default function Person(props) {
     return (
         <div className="p-3 m-5 rounded bg-gradient-to-br from-blue-300 to-blue-500">
-            <p>{props.name}</p>
+            <p><span className="font-bold">{props.title}:</span> {props.name}</p>
             <p><span className="italic">Email:</span> {props.email}</p>
+            <p><span className="italic">Tel.:</span> {props.tel}</p>
         </div>
     )
 }
